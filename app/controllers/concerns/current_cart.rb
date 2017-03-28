07ed878ae9325e2780ef97b03293a9e10e57dd4f
IuliaMada <iulia.valeria.md@gmail.com>
2017-03-28 14:46:38 +0300

    Add add to cart button for store

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -3,10 +3,8 @@ module CurrentCart
 
 	def set_cart
 		@cart = Cart.find(session[:cart_id])
-
-		rescue ActiveRecord::RecordNotFound
-			@cart = Cart.create
-			session[:cart_id] = @cart.cart.id 
-		end
+	rescue ActiveRecord::RecordNotFound
+		@cart = Cart.create
+		session[:cart_id] = @cart.id 
 	end
 end	
