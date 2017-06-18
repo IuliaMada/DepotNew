3e5540c0f9855c5105c84b0544071242ed36f7a0
Iulia <iulia.valeria.md@gmail.com>
2017-06-18 23:25:57 +0300

    internalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   USER, PASSWORD = 'alina', 'alina'
 
-  before_action :authorize
+  before_action :set_i18n_locale_from_params, :authorize
 
 protected
 
@@ -19,4 +19,15 @@ protected
 	    end
 	end	
   end
+
+	def set_i18n_locale_from_params
+		if params[:locale]
+			if I18n.available_locales.map(&:to_s).include?(params[:locale])
+				I18n.locale = params[:locale]
+			else
+				flash.now[:notice] ="#{params[:locale]} translation not available"
+				logger.error flash.now[:notice]
+			end
+		end
+	end
 end
