c3d32dd4c8ffce1a5c5be91363532a9f221ba43c
Iulia <iulia.valeria.md@gmail.com>
2017-06-26 23:05:11 +0300

    Another

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -5,12 +5,10 @@ class StoreController < ApplicationController
 	before_action :set_cart
 
   def index
-  	if session[:counter].nil?
-  		session[:counter] = 1
-  	else
-  		session[:counter] += 1
-  	end
-
-  	@products = Product.order(:title)
+  	if params[:set_locale]
+      redirect_to store_index_url(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+    end
   end
 end
